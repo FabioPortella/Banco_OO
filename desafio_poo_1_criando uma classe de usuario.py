254b5f7f4c6ca7284973729d94c5f2d702330382
FabioPortella <fabioportella2009@gmail.com>
2024-05-30 12:57:24 -0300

    modificando desafio poo 01

--- desafio_poo_1_criando uma classe de usuario.py
+++ desafio_poo_1_criando uma classe de usuario.py
@@ -3,17 +3,14 @@
 # O método `__init__`, irá inicializar os atributos da classe: `nome`, `numero` e `plano`.
 class UsuarioTelefone():
     def __init__(self, nome, numero, plano):
+        # TODO: Aplique o conceito de encapsulamento, onde os atributos serão encapsulados dentro da classe.
         self.nome = nome
         self.numero = numero
-        self.plano = plano
-        
-    # TODO: Aplique o conceito de encapsulamento, onde os atributos serão encapsulados dentro da classe.
-      
-
+        self.plano = plano      
+         
     # A classe `UsuarioTelefone` define um método especial `__str__`, que retorna uma representação em string do objeto.
     def __str__(self):
         return f"Usuário {self.nome} criado com sucesso."
-
 
 # Entrada:
 nome = input()  

3a9f27595c775912a77821dd0e2baf4fc3cd2d43
FabioPortella <fabioportella2009@gmail.com>
2024-05-30 12:06:21 -0300

    primeiro Commit

---
+++ encapsulamento.py
@@ -0,0 +1,16 @@
+class Conta:
+    def __init__(self, saldo=0):
+        self._saldo = saldo
+
+    def depositar(self, valor):
+        # ...
+        self._saldo += valor
+
+    def sacar(self, valor):
+        # ...
+        self._saldo -= valor
+
+
+conta = Conta(100)
+conta.depositar(150)
+print(conta._saldo)
